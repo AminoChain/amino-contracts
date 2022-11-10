f17613192ae2c57dc8e496f5911fb82e0ad2aedd
MaximilianFullStack <mmillionam@gmail.com>
2022-11-10 01:50:37 -0500

    modified:   contracts/AminoChainMarketplace.sol 	modified:   deploy/04-deploy-Marketplace.ts

--- deploy/04-deploy-Marketplace.ts
+++ deploy/04-deploy-Marketplace.ts
@@ -4,7 +4,7 @@ import { ethers } from "hardhat"
 import { developmentChains } from "../helper-hardhat-config"
 import verify from "../utils/verify"
 import { MockERC20, MockERC20__factory } from "../typechain"
-import {delay} from "@nomiclabs/hardhat-etherscan/dist/src/etherscan/EtherscanService";
+import { delay } from "@nomiclabs/hardhat-etherscan/dist/src/etherscan/EtherscanService"
 
 const deployMarketplace: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const { getNamedAccounts, deployments, network } = hre
@@ -31,8 +31,8 @@ const deployMarketplace: DeployFunction = async function (hre: HardhatRuntimeEnv
     } else {
         args = [
             8,
-            "0xb0eaca4246d134cfcd104df91f9cd87e6c7271a7", // USDC
-            "0x2dA81f4520160f6a78660841C4E026d66eC49d6E", // NFT
+            "0x3F7a078C98EB0d0E58EEaC1a2C94183300B345ac", // USDC
+            "0xE6C8610d8C6638fa7088186f29dceC474f1518C2", // NFT
             "0x326C977E6efc84E512bB9C30f76E30c160eD06FB", //LINK on mumbai
             "0x40193c8518BB267228Fc409a613bDbD8eC5a97b3", //Chainlink Oracle on mumbai
         ]
